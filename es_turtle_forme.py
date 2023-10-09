84fd4680ee1d80a00049f71684ff5b895322865f
Patrick.Dutto <patrick.dutto@itiscuneo.eu>
2023-10-09 20:23:19 +0200

    esercizio con turtle
    chiedi numero facce e colore e crea la figura

---
+++ es_turtle_forme.py
@@ -0,0 +1,30 @@
+import turtle 
+
+print ("dimmi il numero di facce della forma:\n")
+
+facce = int (input())
+
+print ("inserisci il colore della forma")
+colore = input()
+
+finestra = turtle.Screen()
+packi = turtle.Turtle() #nome lo decidi tu
+
+packi.speed(facce * 100)
+
+packi.color(colore)
+
+packi.fillcolor(colore)
+
+packi.begin_fill()
+
+for i in range(0, facce):
+    
+    packi.forward((350 + facce)/facce)
+    packi.left(360/facce)
+
+packi.end_fill()
+    
+
+
+finestra.mainloop() #mettilo sempre altrimenti non vedi la finestra 
